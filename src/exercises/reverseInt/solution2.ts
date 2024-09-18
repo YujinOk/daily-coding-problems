9fbbb08a04ae169f599933ad4e70b2ff05f54dc3
YujinOk <yujinok63@gmail.com>
2024-09-18 23:01:36 +0900

    feat: more comments added in solution 2 in reverseInt

--- src/exercises/reverseInt/solution2.ts
+++ src/exercises/reverseInt/solution2.ts
@@ -10,3 +10,8 @@ export const reverseInt = (n: number): number => {
   return parseInt(reversedArr) * (isNegative ? -1 : 1);
 };
 console.log(reverseInt(-543));
+
+// 1. arrNum variable puts the arg into number of array
+// Array.from takes max 3 arg, and 2nd arg is callBack and in this case, it converts Number due to Number constructor
+// 2. number of array gets reversed and joined back in
+// 3. last it gets converted
